990cc4d361a6388e741ffb81dbd0a257c93934de
KirylDv <39598590+KirylDv@users.noreply.github.com>
2019-10-18 12:35:06 +0300

    add: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if( str.length & 1 ){
+        return false;
+    }
+    str = str.split("");
+    let stack = [];
+    str.forEach(elem => {
+        bracketsConfig.forEach(brackets => {
+            if(brackets[0] === brackets[1]){
+                if(elem === brackets[0])
+                    if(stack[stack.length-1] === brackets[0])
+                        stack.pop();
+                    else
+                        stack.push(elem);
+            }
+            else if(elem === brackets[1]){
+                if(stack.length === 0 || stack[stack.length-1] !== brackets[0]){
+                    return false;
+                }
+                stack.pop();
+            }
+            else if(elem === brackets[0]){
+                stack.push(elem);
+            }
+        });
+    });
+    return stack.length === 0;
 }
